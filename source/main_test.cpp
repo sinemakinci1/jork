1eb7843f7894b2650abdfd74c125379bc9bb2afc
Sinem Akinci <99284450+sinemakinci1@users.noreply.github.com>
2024-04-09 13:39:30 -0700

    Update main_test.cpp

--- source/main_test.cpp
+++ source/main_test.cpp
@@ -1,1 +1,19 @@
 
+#include "main.h"
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("GameTest") {
+    SECTION("testDoPet") {
+        // Test petting "marshmallow"
+        game g = game::create();
+        REQUIRE(g.do_pet("marshmallow") == loc(strings::pet_marshmallow));
+
+        // Test petting "cat"
+        REQUIRE(g.do_pet("cat") == loc(strings::pet_marshmallow));
+
+        // Test petting "dog"
+        REQUIRE(g.do_pet("dog") == loc(strings::cannot_pet_that));
+
+    }
+}
+
